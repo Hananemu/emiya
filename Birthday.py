9ebbffe768d55e0d4e93a2b0917665d9f7482e7b
Hananemu <147488578+Hananemu@users.noreply.github.com>
2023-10-11 21:18:20 +0800

    Update Birthday.py

--- Birthday.py
+++ Birthday.py
@@ -1,4 +1,15 @@
-import easygui
+import tkinter as tk
 import time
-easygui.msgbox(msg="woyouyiyuzheng")
+
+# 创建主窗口
+root = tk.Tk()
+root.title("等待一秒后关闭窗口")
+
+# 等待一秒
 time.sleep(1)
+
+# 关闭窗口
+root.destroy()
+
+# 进入主循环
+root.mainloop()
