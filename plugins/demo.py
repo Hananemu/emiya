450acef2a1c56c5914fc1714dc4450412f95cd39
Hananemu <147488578+Hananemu@users.noreply.github.com>
2023-10-11 20:23:53 +0800

    Update demo.py

--- plugins/demo.py
+++ plugins/demo.py
@@ -1,6 +1,7 @@
 from libs.event.qqvent import oncommond
 import os
+import ping
 @oncommond (promat=[""],cmd=["生日"]
 def handle(n):
     os.system("python.Birthday.py")
-    
+    sendGroupMessage("启动成功",n.groupID)
