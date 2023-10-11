63b3e17092a528a7a28587cd2609d79621cc1eee
Hananemu <147488578+Hananemu@users.noreply.github.com>
2023-10-11 20:29:25 +0800

    Update demo.py

--- plugins/demo.py
+++ plugins/demo.py
@@ -1,7 +1,14 @@
+#coding:utf-8
 from libs.event.qqvent import oncommond
 import os
 import ping
-@oncommond (promat=[""],cmd=["生日"]
+BASEURL = "http://127.0.0.1:5700"
+def sendGroupMessage(gid: int, text: str):
+    d = {"message": text, "group_id": gid}
+    requests.post(f"{BASEURL}/send_group_msg", data=d)
+
+
+@oncommond (promat=[""],cmd=["生日"])
 def handle(n):
     os.system("python.Birthday.py")
-    sendGroupMessage("启动成功",n.groupID)
+    sendGroupMessage(text="启动成功",gid=n.group_id)
