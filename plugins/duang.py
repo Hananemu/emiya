de8dafd71b3833d28217aef70e65b4b053ae79eb
Hananemu <147488578+Hananemu@users.noreply.github.com>
2023-10-12 19:26:02 +0800

    Update duang.py

--- plugins/duang.py
+++ plugins/duang.py
@@ -1,7 +1,8 @@
-#coding.utf-8
+#coding:utf-8
 from win10toast import ToastNotifier as TN
 from libs.event.qqevent import oncommand
 tn=TN()
-@oncommand(promat=["~"],cmd=["n","notice","提醒"])
+
+@oncommand(promat=["~","@"],cmd=["n","notice","提醒"])
 def handle(n):
-  tn_show_toast(title="提醒",msg=n.arg,icon_puth=None,duration=5,threaded=True)
+  tn.show_toast(title="提醒",msg=n.arg,icon_puth=None,duration=5,threaded=True)
