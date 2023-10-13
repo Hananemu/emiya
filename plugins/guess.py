a3a8715c74923ca1ed8a743d3f2345b792d4bde9
Hananemu <147488578+Hananemu@users.noreply.github.com>
2023-10-13 20:35:35 +0800

    Update guess.py

--- plugins/guess.py
+++ plugins/guess.py
@@ -1,17 +1,24 @@
 from libs.event.qqvent import oncommand
 import random
+import requests
 BASEURL = "http://127.0.0.1:5700"
-def sendGroupMessage(gid: int, text: str):
+def send(gid: int, text: str):
     d = {"message": text, "group_id": gid}
     requests.post(f"{BASEURL}/send_group_msg", data=d)
     
-random_sum=random.randint(-100,100)#随机生成一个负一百到一百的整数
+sum=random.randint(-100,100)#随机生成一个负一百到一百的整数
 @oncommand(promat=["","/"],cmd=["猜",“c”])
 def handle(n):
-    sendGroupMessage(text="开始游戏啦",gid=n.group_id)
+    send(text="开始游戏啦",gid=n.group_id)
     
     num=n.arg
-    num=int(n.arg)
+    try:
+        num=int(n.arg.replace(" ",""))
+    except Exception as e:
+        send(n.group_id,e)
+        return
+        
+    
     if num>sum:
       send(n.groud_id,text=("你猜大了!"))
     elif num<sum:
