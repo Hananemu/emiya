de440b8d7ff125d7def05392ed02ef30f700e2db
Hananemu <147488578+Hananemu@users.noreply.github.com>
2023-10-13 20:37:57 +0800

    Update guess.py

--- plugins/guess.py
+++ plugins/guess.py
@@ -1,3 +1,4 @@
+#coding:gbk
 from libs.event.qqvent import oncommand
 import random
 import requests
@@ -7,7 +8,7 @@ def send(gid: int, text: str):
     requests.post(f"{BASEURL}/send_group_msg", data=d)
     
 sum=random.randint(-100,100)#随机生成一个负一百到一百的整数
-@oncommand(promat=["","/"],cmd=["猜",“c”])
+@oncommand(promat=["@","/"],cmd=["猜",“c”])
 def handle(n):
     send(text="开始游戏啦",gid=n.group_id)
     
@@ -20,9 +21,9 @@ def handle(n):
         
     
     if num>sum:
-      send(n.groud_id,text=("你猜大了!"))
+      send(n.groud_id,text="你猜大了!")
     elif num<sum:
-      send(n.groud_id,text=("你猜小了!"))
+      send(n.groud_id,text="你猜小了!")
     else if num==sum:
-      send(n.groud_id,text=("你猜对了！"))
-    random_sum=random.randint(-100,100)
+      send(n.groud_id,text="你猜对了！")
+      sum=random.randint(-100,100)
