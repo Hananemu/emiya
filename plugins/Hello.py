12a2b2750fbf5c5c35b5b0a321512979f9a0cd96
Hananemu <147488578+Hananemu@users.noreply.github.com>
2023-10-13 21:28:37 +0800

    Create Hello.py

---
+++ plugins/Hello.py
@@ -0,0 +1,13 @@
+from libs.event.qqvent import oncommand
+import requests
+BASEURL = "http://127.0.0.1:5700"
+def sendGroupMessage(gid: int, text: str):
+    d = {"message": text, "group_id": gid}
+    requests.post(f"{BASEURL}/send_group_msg", data=d)
+
+@oncommand(promat=(""),cmd=("启动"))
+def handle(n):
+  try:
+    sendGroupMsg(gid=n.group_id,text="Hello World!")
+  except:
+    pass
